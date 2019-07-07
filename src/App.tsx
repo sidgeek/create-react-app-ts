5d2ae2219c2afa0594868ee4bc63d8045dff7668
sidgeek <sid.shi@nirvana-info.com>
2019-07-08 00:06:32 +0800

    add new ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Description from "./Description";
-import Header from "./Header";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Description from "./Description";
+// import Header from "./Header";
+// import logo from "./logo.svg";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import NewTicket from "./pages/newTicket";
 import "./App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Header name="REACT" />
-        </header>
-        <Description countBy={3} />
+        </header> */}
+        {/* <Description countBy={3} /> */}
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/newTicket" component={NewTicket} />
         {/* <Route exact path="/" render={Home} />
         <Route exact path="/about" render={() => <About />} /> */}
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { any } from 'prop-types';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
