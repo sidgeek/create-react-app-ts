4631f4935381e0debeb69fc986f807a5d6cf159c
sidgeek <sid.shi@nirvana-info.com>
2019-07-08 08:38:45 +0800

    remove some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Description from "./Description";
-// import Header from "./Header";
-// import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NewTicket from "./pages/newTicket";
 import "./App.css";
 
+const Header: React.FC = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </div>
+);
+
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Header name="REACT" />
-        </header> */}
-        {/* <Description countBy={3} /> */}
+        <Header />
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/newTicket" component={NewTicket} />
